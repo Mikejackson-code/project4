ae0275b9beb13cd6e58b6a47949406d193b3126f
Mike Jackson <jackson-mike@sbcglobal.net>
2020-05-20 21:36:45 -0400

    Last run

---
+++ course-03/exercises/udacity-c3-deployment/docker/set_env.sh
@@ -0,0 +1,8 @@
+$env:POSTGRESS_USERNAME='jacksonudagram'
+$env:POSTGRESS_PASSWORD='jacksonudagram'
+$env:POSTGRESS_HOST='jacksonudagram.cm4xlcca34ef.us-east-2.rds.amazonaws.com'
+$env:POSTGRESS_DB='jacksonudagram'
+$env:AWS_REGION='us-east-2'
+$env:AWS_PROFILE=default
+$env:AWS_BUCKET='jacksonudagram'
+$env:JWT_SECRET='helloworld'
